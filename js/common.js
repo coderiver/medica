1092610ca00a46be50f23ec759eb632786026ec9
etna-scorpio <etna.scorpio@gmail.com>
2014-06-24 17:12:16 +0300

    product search finished

--- js/common.js
+++ js/common.js
@@ -2,4 +2,7 @@ head.ready(function() {
 	$('.dropdown select').click(function(event) {
 		$(this).next().val($(this).val());
 	});
+	$('.last select').click(function(event) {
+		$(this).next.addClass('n')().val($(this).val());
+	});
 });
