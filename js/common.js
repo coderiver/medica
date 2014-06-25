760c2e0e2d89e2ded98673d3b5889b80cd4c938d
etna-scorpio <etna.scorpio@gmail.com>
2014-06-25 11:16:11 +0300

    sidebar menu is added small fixes in index file

--- js/common.js
+++ js/common.js
@@ -2,7 +2,16 @@ head.ready(function() {
 	$('.dropdown select').change(function(event) {
 		$(this).next().val($(this).val());
 	});
+
 	$('.sort select').change(function(event) {
 		$(this).next().children('a').text($(this).val());
+	});
+
+	$('.auth__menu').click(function(event) {
+		$('.header__sidebar').addClass('is-active');
+	});
+
+	$('.close').click(function(event) {
+		$('.header__sidebar').removeClass('is-active');
 	});
 });
