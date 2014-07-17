32da1c4f28ff96adf587a53a23c5ac88d05a4dce
etna-scorpio <etna.scorpio@gmail.com>
2014-07-17 15:04:25 +0300

    main page slider height fix (for large screen)

--- js/common.js
+++ js/common.js
@@ -8,15 +8,41 @@ head.ready(function() {
 	});
 
 	$('.auth__menu').click(function(event) {
-		$('.header__sidebar').addClass('is-active');
+		$('.header__sidebar').show();
 	});
 
 	$('.close').click(function(event) {
-		$('.header__sidebar').removeClass('is-active');
+		$('.header__sidebar').hide();
 	});
 
 	$('.prinfo__text .read').click(function(event) {
 		$('.more').slideDown();
 		$('.prinfo__text .read').hide();
 	});
+
+	//main slider on the whole size of the window screen
+	function divHeight() {
+	  var windowHeight = $(window).height();
+	  $(".js-main-screen").css({
+	   height: windowHeight
+	  });
+	 }
+	 if ($(window).width()>860 && $(window).height()>860) {
+	 	divHeight();
+	 }	 else {
+	 	$(".js-main-screen").css({
+	   height: 'auto'
+	  });
+	 }
+
+	 $(window).resize(function(){
+	 	if ($(window).width()>860 && $(window).height()>860) {
+	 		divHeight();
+	 	}	 else {
+	 		$(".js-main-screen").css({
+	 	  height: 'auto'
+	 	 });
+	 	}
+	 });
 });
+
